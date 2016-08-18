aa65e132f53838522d28f4a309f6eaf3e0583975
PilockHulmes <392206976@qq.com>
2016-08-18 23:17:02 +0800

    add some util functions

--- assets/js-script/utils.js
+++ assets/js-script/utils.js
@@ -1,3 +1,7 @@
+const sleep = (time) => {
+  // use c++ to implement sleep
+}
+
 const random = (start, end) => {
   if (start <= end) {
     throw(`End(${end}) must be greater than start(${start})`)
@@ -6,6 +10,24 @@ const random = (start, end) => {
   return Math.round(Math.random() * rate + start)
 }
 
+const run = (func, sleep, bounce) => {
+  bounce = bounce ? bounce : 0
+  const sleepTime = random(sleep - bounce, sleep + bounce)
+  func()
+  sleep(sleepTime)
+  return arguments.callee // this function itself
+}
+
+const wrapper = (...params) => {
+  const func = params.shift()
+  return () => {
+    func(...params)
+  }
+}
+
 module.exports = {
   random,
+  sleep,
+  run,
+  wrapper,
 }
