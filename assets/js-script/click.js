2099bce15d450f3f6c4c63bc69d06c89ea6e825f
Woody Hu <woodyhu@augmentum.com.cn>
2016-08-19 18:37:55 +0800

    add move and drag functions

--- assets/js-script/click.js
+++ assets/js-script/click.js
@@ -1,11 +1,17 @@
 import utils from './utils.js'
 
-const clickInterval = [30, 70]
+const clickInterval = [60, 130]
 
 // functinos that enable click
 const mouseDown = (point) => {
   // mock mouse down
   console.log(`mouse down at ${point.x}, ${point.y}`)
+}
+
+const mouseMove = (src, dest) => {
+  // mock mosue move
+  // maybe use Bézier curve to mock the path
+  console.log(`mouse move from ${src.x}, ${src.y} to ${dest.x}, ${dest.y}`)
 }
 
 const mouseUp = (point) => {
@@ -22,6 +28,16 @@ const click = (point) => {
   setTimeout(() => {
     mouseUp(point)
   }, randomSec)
+}
+
+const drag = (src, dest) => {
+  mouseDown(src)
+
+  // wait some time
+  mouseMove(src, dest)
+
+  // wait some time
+  mouseUp(dest)
 }
 
 module.exports = {
